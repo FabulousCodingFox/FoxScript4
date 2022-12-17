356af4ee2d19288eaffe8fa45f34a86581ed4990
FabulousFox <78906517+FabulousCodingFox@users.noreply.github.com>
2022-12-17 15:49:43 +0100

    Updated Logger.py

--- core/Compiler.py
+++ core/Compiler.py
@@ -1,15 +1,26 @@
 from compile.McFunction import McFunction
 from util.Logger import Logger
+import os
 
 class Compiler(object):
     def __init__(self, pt:str) -> None:
         self.functions:list = []
         self.projectPath:str = pt
 
+        #Check if the project exists
+
+        if not os.path.exist(pt):
+            Logger.error("COMPILER INIT", "The path to the project does not exist")
+            exit()
+
+        if not os.path.exist(os.path.join(pt, "settings.json")):
+            Logger.error("COMPILER INIT", "The path does not contain a project.json")
+            exit()
+
     def addFunction(self, func:McFunction) -> None:
         self.functions.append(func)
     
-    def compile(self) -> None:
+    def compile(self) -> bool:
 
         while True:
             c = True
--- core/util/Logger.py
+++ core/util/Logger.py
@@ -4,15 +4,19 @@ from colorama import Style
 from datetime import datetime
 
 class Logger:
-    def _prepareMessage(col:Fore, stage:str, txt:str) -> str:
+    def _prepareMessage(col:Fore, state:str, stage:str, txt:str) -> str:
         currentDateAndTime = datetime.now().strftime("%H:%M:%S")
-        return f"{col}[{currentDateAndTime}][{stage}]: {Style.RESET_ALL}{txt}"
+        return f"{col}[{currentDateAndTime}][{state}][{stage}]: {Style.RESET_ALL}{txt}"
 
     def init() -> None:
         colorama_init()
 
     def log(stage:str, txt:str) -> None:
-        print(Logger._prepareMessage(Fore.WHITE, stage, txt))
+        print(Logger._prepareMessage(Fore.WHITE, "LOG", stage, txt))
     
     def error(stage:str, txt:str) -> None:
-        print(Logger._prepareMessage(Fore.RED, stage, txt))
+        print(Logger._prepareMessage(Fore.RED, "ERROR", stage, txt))
+
+    def warning(stage:str, txt:str) -> None:
+        print(Logger._prepareMessage(Fore.YELLOW, "WARNING", stage, txt))
+
